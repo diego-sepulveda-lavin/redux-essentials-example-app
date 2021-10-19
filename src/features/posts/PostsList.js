87d588bdba95a1e4e78c17af186e95047009998b
Diego Sepulveda <diegosl@hotmail.cl>
2021-10-19 20:32:18 -0300

    async logic added

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,16 +1,16 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import { PostAuthor } from './PostAuthor';
 import { TimeAgo } from './TimeAgo';
 import { ReactionButtons } from './ReactionButtons';
+import { Spinner } from '../../components/Spinner';
 
-export const PostsList = () => {
-  const posts = useSelector(state => state.posts);
+import { selectAllPosts, fetchPosts } from './postsSlice';
 
-  const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
-
-  const renderedPosts = orderedPosts.map(post => (
+export const PostExcerpt = ({ post }) => {
+  return (
     <article className='post-excerpt' key={post.id}>
       <h3>{post.title}</h3>
       <div>
@@ -23,12 +23,37 @@ export const PostsList = () => {
         View Post
       </Link>
     </article>
-  ));
+  );
+};
 
+export const PostsList = () => {
+  const dispatch = useDispatch();
+
+  const posts = useSelector(selectAllPosts);
+  const postStatus = useSelector(state => state.posts.status);
+  const error = useSelector(state => state.posts.error);
+
+
+  useEffect(() => {
+    if (postStatus === 'idle') {
+      dispatch(fetchPosts());
+    }
+  }, [dispatch, postStatus]);
+
+  let content;
+
+  if (postStatus === 'loading') {
+    content = <Spinner text='Loading...' />;
+  } else if (postStatus === 'succeeded') {
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date));
+    content = orderedPosts.map((post) => <PostExcerpt key={post.id} post={post} />);
+  } else if (postStatus === 'failed') {
+    content = <div>{error}< /div>;
+  }
   return (
     <section className='posts-list'>
       <h2>Posts</h2>
-      {renderedPosts}
+      {content}
     </section>
   );
 };
